0a255b6c58e36456f1351674a8fc3f2bff439245
g-yonchev <georgi.d.yonchev@gmail.com>
2015-01-16 03:01:46 +0200

    TelerikAcademy Homework Intro Programming

---
+++ Homeworks/01.IntroProgrammingHW/08.SquareRoot/SquareRoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        /*  Problem 8. Square Root
+
+            Create a console application that calculates and prints the square root of the number 12345.
+        */
+
+        Console.Title = "Square root";
+
+        double number = Math.Sqrt(12345);
+        Console.WriteLine("The square root of the number 12345 is = {0}", number);
+    }
+}
---
+++ Homeworks/01.IntroProgrammingHW/14.CurrentDateAndTime/CurrentDateAndTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class CurrentDateAndTime
+{
+    static void Main()
+    {
+        /*  Problem 14.* Current Date and Time
+
+            Create a console application that prints the current date and time. Find out how in Internet.
+        */
+
+        Console.Title = "Current time";
+
+        Console.WriteLine("The time is: {0}", DateTime.Now);
+    }
+}
